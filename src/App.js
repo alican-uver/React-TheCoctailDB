74ec9f1fed86884687e6c65a5720601cf8dd3b9a
Alican Uver <can.uverr@gmail.com>
2020-06-17 19:23:19 +0300

    navbar created

--- src/App.js
+++ src/App.js
@@ -7,13 +7,17 @@ import About from './pages/About';
 import NotFound from './pages/NotFound';
 import SingleCoctail from './pages/SingleCoctail';
 
+// Navbar
+import Navbar from './components/Navbar';
+
 function App() {
   return (
     <Router>
+      <Navbar />
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/about" component={About} />
-        <Route exact path="/singleCoctail" component={SingleCoctail} />
+        <Route exact path="/cocktail/:id" component={SingleCoctail} />
         <Route path="*" component={NotFound} />
       </Switch>
     </Router>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from "react";
+import { Menu } from "antd";
+import logo from "../logo/coctailLogo.png";
+import { Link } from 'react-router-dom';
+import {HomeOutlined} from '@ant-design/icons';
 
 const Navbar = () => {
-    return (
-        <div>
-            Navbar.js
-        </div>
-    )
-}
+  return (
+    <Menu mode="horizontal" theme = "dark" style ={{display:'flex', justifyContent:'space-between', alignItems: 'center'}}>
+      <Menu.Item><img src= {logo} alt="cocktail logo" width = "30"/>The Cocktail DB</Menu.Item>
+      <Menu.Item><HomeOutlined /><Link to ='/'>Home</Link></Menu.Item>
+      <Menu.Item><Link to ='/about'>About</Link></Menu.Item>
+    </Menu>
+  );
+};
 
-export default Navbar
+export default Navbar;
