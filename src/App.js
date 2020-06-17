a3c937fce40a1f96a4d9d9ecb80d37be2606ebeb
Alican Uver <can.uverr@gmail.com>
2020-06-17 18:29:17 +0300

    react-router-dom init

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+// Pages
+import Home from './pages/Home';
+import About from './pages/About';
+import NotFound from './pages/NotFound';
+import SingleCoctail from './pages/SingleCoctail';
 
 function App() {
   return (
-    <div>
-
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/singleCoctail" component={SingleCoctail} />
+        <Route path="*" component={NotFound} />
+      </Switch>
+    </Router>
   );
 }
 
