6cae33f29682ccf58d52fb505d22220c60ab50d9
Alican Uver <can.uverr@gmail.com>
2020-06-18 22:50:50 +0300

    getCoctails function and states created

--- src/context/ContextCoctail.js
+++ src/context/ContextCoctail.js
@@ -1,17 +1,52 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState, useEffect } from 'react';
 
 export const ContextCoctail = createContext();
 
 export const CoctailProvider = props => {
-    const [coctail, setCoctail] = useState(['margarita', 'sex on the beach']);
+    const [coctails, setCoctails] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [searchTerm, setSearchTerm] = useState("mar");
+
+    useEffect(() => {
+        setLoading(true);
+        async function getDrinks() {
+            try {
+                const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchTerm}`)
+                const data = await response.json();
+                const { drinks } = data;
+                if (drinks) {
+                    const newCoctails = drinks.map(item => {
+                        const { idDrink, strDrink, strDrinkThumb, strAlcoholic, strGlass } = item;
+                        return {
+                            id: idDrink,
+                            name: strDrink,
+                            image: strDrinkThumb,
+                            info: strAlcoholic,
+                            glass: strGlass
+                        }
+                    })
+                    setCoctails(newCoctails);
+                }
+                else {
+                    setCoctails([]);
+                }
+            }
+            catch (error) {
+                console.log(error)
+            }
+            setLoading(false);
+        }
+        getDrinks();
+    }, [searchTerm])
 
     const contextValues = {
-        coctail, setCoctail
+        coctails, setCoctails, loading, setLoading,
+        searchTerm, setSearchTerm
     }
 
     return (
         <>
-            <ContextCoctail.Provider value = {contextValues}>
+            <ContextCoctail.Provider value={contextValues}>
                 {props.children}
             </ContextCoctail.Provider>
         </>
