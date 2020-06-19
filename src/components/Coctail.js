3c4e93b949bc4b7a176235cb8daff03d15abb035
Alican Uver <can.uverr@gmail.com>
2020-06-19 23:03:20 +0300

    src/components/CoctailList.js
    loading text removed, spin added

--- src/components/Coctail.js
+++ src/components/Coctail.js
@@ -1,20 +1,12 @@
 import React from "react";
 import { Card } from "antd";
-import { Link } from "react-router-dom";
 
-const Coctail = props => {
-  const { name, image, info, id, glass } = props;
+const Coctail = (props) => {
+  const { name, image, info, glass } = props;
 
   return (
     <Card hoverable cover={<img src={image} alt="coctail" />}>
-      <Card.Meta
-        title={name}
-        description={glass}
-        style={{ marginBottom: "10px" }}
-      />
-      <small>{info}</small>
-      <br />
-      <Link to={`/cocktail/${id}`}>More Details</Link>
+      <Card.Meta title={name} description={glass} />
     </Card>
   );
 };
--- src/components/CoctailList.js
+++ src/components/CoctailList.js
@@ -1,44 +1,30 @@
-import React, { useContext } from "react";
-import Coctail from "./Coctail";
-import { ContextCoctail } from "../context/ContextCoctail";
-import { Row, Col, Spin } from "antd";
-import styled from 'styled-components';
+import React, { useContext } from 'react';
+import Coctail from './Coctail';
+import { ContextCoctail } from '../context/ContextCoctail';
+import { Row, Col } from 'antd';
 
 const CoctailList = () => {
-  const { loading, coctails } = useContext(ContextCoctail);
+    const { loading, coctails } = useContext(ContextCoctail);
 
-  if (loading) {
+    if (loading) {
+        return <h2 style={{ textAlign: 'center' }}>Loading Coctails ...</h2>
+    }
+
+    if (coctails.length < 1) {
+        return <h2>no coctails matched your search criteria</h2>
+    }
+
     return (
-      <SpinWrapper>
-        <Spin size="large"></Spin>
-      </SpinWrapper>
-    );
-  }
+        <Row gutter={[24, 40]} style={{ width: '90%', margin: 'auto' }}>
+            {
+                coctails.map(item => {
+                    return <Col lg={6} md={8} xs={12} key={item.id} >
+                        <Coctail {...item} />
+                    </Col>
+                })
+            }
+        </Row>
+    )
+}
 
-  if (coctails.length < 1) {
-    return <h2>no coctails matched your search criteria</h2>;
-  }
-
-  return (
-    <Row gutter={[24, 40]} style={{ width: "90%", margin: "auto" }}>
-      {coctails.map((item) => {
-        return (
-          <Col lg={6} md={8} xs={12} key={item.id}>
-            <Coctail {...item} />
-          </Col>
-        );
-      })}
-    </Row>
-  );
-};
-
-export default CoctailList;
-
-
-const SpinWrapper = styled.div`
-  text-align: "center";
-  min-height: calc(100vh - 46px);
-  display:flex;
-  justify-content:center;
-  align-items:center;
-`
+export default CoctailList
