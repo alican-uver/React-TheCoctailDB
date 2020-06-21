1ef4fa5da0fbccca941c2f6187383c731bab475d
Alican Uver <can.uverr@gmail.com>
2020-06-21 15:18:13 +0300

    ID props retrieved and link added

--- src/components/Coctail.js
+++ src/components/Coctail.js
@@ -1,12 +1,16 @@
 import React from "react";
-import { Card } from "antd";
+import { Card, Button } from "antd";
+import { Link } from 'react-router-dom';
+import { InfoCircleOutlined } from '@ant-design/icons';
 
 const Coctail = (props) => {
-  const { name, image, info, glass } = props;
+  const { name, image, info, id, glass } = props;
 
   return (
     <Card hoverable cover={<img src={image} alt="coctail" />}>
-      <Card.Meta title={name} description={glass} />
+      <Card.Meta title={name} description={glass} style={{ marginBottom: '10px' }} />
+      <small>{info}</small><br />
+      <Link to={`/cocktail/${id}`}> <Button type="primary" icon={<InfoCircleOutlined />}>More Info</Button></Link>
     </Card>
   );
 };
