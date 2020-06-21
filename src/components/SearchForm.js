d870aaf5d71aab791731bd27b6a6e83305235f43
Alican Uver <can.uverr@gmail.com>
2020-06-21 19:08:26 +0300

    component styled and functions written

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,14 +1,37 @@
-import React, { useContext } from 'react'
-import { ContextCoctail } from '../context/ContextCoctail';
+import React, { useContext, useRef, useEffect } from "react";
+import { ContextCoctail } from "../context/ContextCoctail";
+import { Input } from 'antd';
 
 const SearchForm = () => {
-    const { setSearchTerm } = useContext(ContextCoctail);
+  const { searchTerm ,setSearchTerm } = useContext(ContextCoctail);
+  const searchValue = useRef("");
+ 
+  useEffect(() => {
+    searchValue.current.focus();
+  }, []);
 
-    return (
-        <div>
-            SearchForm
-        </div>
-    )
-}
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
 
-export default SearchForm
+  const searchCocktail = e => {
+    setSearchTerm(e.target.value);
+  };
+
+  return (
+    <section>
+      <h2 style = {{textAlign:'center'}}>Search Coctails</h2>
+      <form onSubmit={handleSubmit} style ={{width:'90%', margin:'auto'}}>
+          <Input
+            placeholder = "Search your favourite cocktail"
+            size ="large"
+            value = {searchTerm}
+            onChange={searchCocktail}
+            ref={searchValue}
+          />
+      </form>
+    </section>
+  );
+};
+
+export default SearchForm;
